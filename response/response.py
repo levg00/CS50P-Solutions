99c4f328129274b10be0a95a7a9e1bc2fc8a3d08
levg00 <9055577224g@gmail.com>
2026-03-31 21:34:21 +0300

    Add files via upload

---
+++ response/response.py
@@ -0,0 +1,10 @@
+import validators
+
+def main():
+    email = input("What's your email address? ").strip()
+    if validators.email(email):
+        print("Valid")
+    else:
+        print("Invalid")
+
+main()
